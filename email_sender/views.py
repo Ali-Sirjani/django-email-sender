16923bc6253b3c641f10a81eb50ffe84f22f8fda
Ali Sirjani <alisirjani1234@gmail.com>
2024-10-23 00:37:36 +0330

    refactor: enhance MessagePanelView to support AJAX filtering and pagination, and queryset refinement
    - Refactored `MessagePanelView` to filter messages by subject (`q` parameter) for both AJAX and non-AJAX requests.
    - Added `is_ajax` method to check for AJAX requests.
    - Updated `get_context_data` to load formsets only for non-AJAX requests.
    - Implemented JSON response for message list and pagination when using AJAX.
    - Refined the queryset to only display sent messages (`is_sent=True`).

--- email_sender/views.py
+++ email_sender/views.py
@@ -1,3 +1,5 @@
+import json
+
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
@@ -5,10 +7,12 @@ from django.views.decorators.http import require_POST
 from django.db import transaction
 from django.contrib import messages
 from django.utils.translation import gettext_lazy as _
+from django.http import JsonResponse
 
 from .models import MsgRecord, MsgRecordImage, MsgRecordFile
 from .forms import MsgRecordForm
 from .mixins import JSONResponseMixin
+from .utils import DateTimeEncoder
 
 
 class MessagePanelView(LoginRequiredMixin, generic.ListView):
@@ -16,15 +20,48 @@ class MessagePanelView(LoginRequiredMixin, generic.ListView):
     paginate_by = 10
     template_name = 'email_sender/panel_msg.html'
     context_object_name = 'messages'
+    queryset = MsgRecord.objects.filter(is_sent=True)
 
-    def get_context_data(self, *, object_list=None, **kwargs):
+    def get_queryset(self):
+        queryset = super().get_queryset()
+
+        q = self.request.GET.get('q')
+        if q:
+            queryset = queryset.filter(subject__icontains=q).distinct()
+
+        return queryset
+
+    def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
-        context['msg_record_img_formset'] = MsgRecordImage.get_img_formset()
-        context['msg_record_file_formset'] = MsgRecordFile.get_file_formset()
-        context['msg_record_form'] = MsgRecordForm()
+        if not self.is_ajax():
+            context['msg_record_img_formset'] = MsgRecordImage.get_img_formset()
+            context['msg_record_file_formset'] = MsgRecordFile.get_file_formset()
+            context['msg_record_form'] = MsgRecordForm()
 
         return context
+
+    def is_ajax(self):
+        return self.request.headers.get('x-requested-with') == 'XMLHttpRequest'
+
+    def get(self, request, *args, **kwargs):
+        if self.is_ajax():
+            self.object_list = self.get_queryset()
+            context = self.get_context_data(**kwargs)
+            page_obj = context['page_obj']
+
+            return JsonResponse({
+                'messages': json.dumps(list(context['messages'].values('id', 'subject', 'datetime_created')),
+                                       cls=DateTimeEncoder, date_format='%Y/%m/%d at %H:%M'),
+                'page': page_obj.number,
+                'total_pages': page_obj.paginator.num_pages,
+                'has_previous': page_obj.has_previous(),
+                'previous_page_number': page_obj.previous_page_number() if page_obj.has_previous() else None,
+                'has_next': page_obj.has_next(),
+                'next_page_number': page_obj.next_page_number() if page_obj.has_next() else None
+            })
+
+        return super().get(request, *args, **kwargs)
 
 
 @login_required
@@ -56,7 +93,8 @@ def send_and_save_mgs_view(request):
     response_data = {
         'msg_form': json_mixin_obj.ajax_response_form(msg_form),
         'msg_img_formset': [json_mixin_obj.ajax_response_form(msg_img_form) for msg_img_form in msg_img_formset.forms],
-        'msg_file_formset': [json_mixin_obj.ajax_response_form(msg_file_form) for msg_file_form in msg_file_formset.forms],
+        'msg_file_formset': [json_mixin_obj.ajax_response_form(msg_file_form) for msg_file_form in
+                             msg_file_formset.forms],
     }
 
     return json_mixin_obj.render_to_json_response(response_data, status=400)
